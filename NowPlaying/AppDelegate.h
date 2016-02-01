fc73ac6ba9805ee50c0654cfa2a63f1587586513
김승호 <niceb5y@gmail.com>
2016-02-01 18:25:39 +0900

    initial commit

--- NowPlaying/AppDelegate.h
+++ NowPlaying/AppDelegate.h
@@ -7,9 +7,15 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <ScriptingBridge/ScriptingBridge.h>
+#import "iTunes.h"
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSSharingServiceDelegate, NSSharingServicePickerDelegate>
 
+@property (weak) IBOutlet NSTextFieldCell *nowplayingText;
+@property (weak) IBOutlet NSImageView *imageView;
+@property (weak) IBOutlet NSButton *button;
+- (IBAction)share:(id)sender;
 
 @end
 
--- NowPlaying/AppDelegate.m
+++ NowPlaying/AppDelegate.m
@@ -11,16 +11,52 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (weak) iTunesApplication *iTunes;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	// Insert code here to initialize your application
+	[_button sendActionOn:NSLeftMouseDownMask];
+	[_button setImage:[NSImage imageNamed:NSImageNameShareTemplate]];
+	[self update];
+	[NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(update) userInfo:nil repeats:YES];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
 	// Insert code here to tear down your application
+}
+
+- (IBAction)share:(id)sender {
+	iTunesApplication *iTunes = (iTunesApplication *)[[SBApplication alloc] initWithBundleIdentifier:@"com.apple.iTunes"];
+	iTunesTrack *track = iTunes.currentTrack;
+	NSImage *image = track.artworks[0].data;
+	NSMutableArray *array = [NSMutableArray arrayWithObject:[NSString stringWithFormat:@"#NowPlaying %@ - %@ - %@", track.artist, track.album, track.name]];
+	if (image) [array addObject:image];
+	NSSharingServicePicker *picker = [[NSSharingServicePicker alloc] initWithItems:array];
+	picker.delegate = self;
+	[picker showRelativeToRect:[sender bounds] ofView:sender preferredEdge:NSMinYEdge];
+}
+
+- (void)update {
+	iTunesApplication *iTunes = (iTunesApplication *)[[SBApplication alloc] initWithBundleIdentifier:@"com.apple.iTunes"];
+	iTunesTrack *track = iTunes.currentTrack;
+	_imageView.image = track.artworks[0].data;
+	[_nowplayingText setTitle:[NSString stringWithFormat:@"%@ - %@ - %@", track.artist, track.album, track.name]];
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+	return YES;
+}
+
+- (id <NSSharingServiceDelegate>)sharingServicePicker:(NSSharingServicePicker *)sharingServicePicker delegateForSharingService:(NSSharingService *)sharingService {
+	return self;
+}
+
+- (NSWindow *)sharingService:(NSSharingService *)sharingService sourceWindowForShareItems:(NSArray *)items sharingContentScope:(NSSharingContentScope *)sharingContentScope {
+	return self.window;
 }
 
 @end
